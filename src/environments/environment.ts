0db1bc00e7947bddf231e140f380d71f27b65527
Oddbjørn Borge-Jensen <oddbjorn.sb@gmail.com>
2022-08-26 15:10:58 +0200

    CHORE: Added pokemon api endpoint to environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   apiTrainers: "https://osb-assignment-api.herokuapp.com/trainers",
+  apiPokemon: "https://pokeapi.co/api/v2/pokemon",
   apiKey: "JamCxYOrl3ZpOQwlJvyrMgykZCmzsmjBoO0INg7LoOhIboGHLz55j6624LmdR3g1"
 };
