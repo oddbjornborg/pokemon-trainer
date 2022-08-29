fdec2830c2c0435930a15837c02955ae9bc53bcd
Oddbjørn Borge-Jensen <oddbjorn.sb@gmail.com>
2022-08-29 12:44:02 +0200

    FEAT: Added proper register functionality and some general login styling

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterService } from 'src/app/services/register.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+  get registered(): boolean {
+    return this.registerService.registered;
+  }
+
+  get error(): string | undefined {
+    return this.registerService.error;
+  }
+
+  get loading(): boolean {
+    return this.registerService.loading;
+  }
+
+  constructor(
+    private readonly registerService: RegisterService
+  ) { }
 
   ngOnInit(): void {
+    this.registerService.registered = false;
   }
 
 }
--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
@@ -11,12 +11,43 @@ const { apiKey, apiTrainers } = environment
 })
 export class RegisterService {
 
+  private _registered: boolean = false;
+  private _error?: string;
+  private _loading: boolean = false;
+  
+  get registered(): boolean {
+    return this._registered;
+  }
+
+  set registered(newValue: boolean) {
+    this._registered = newValue;
+  }
+
+  get error(): string | undefined {
+    return this._error;
+  }
+
+  get loading(): boolean {
+    return this._loading;
+  }
+
   constructor(
     private readonly http: HttpClient
   ) { }
 
   public register(username: string) {
-    this.createTrainer(username);
+    this.createTrainer(username)
+      .subscribe({
+        next: () => {
+          console.log("Registered new user " + username);
+          this._registered = true;
+        },
+        error: (err: HttpErrorResponse) => {
+          this._error = err.message;
+          console.log(this._error);
+          this._registered = false;
+        }
+      })
   }
 
   private createTrainer(username: string): Observable<Trainer> {
